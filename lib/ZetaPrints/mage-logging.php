23da2f990d0a1524d17e1578e2e94923c6ecab0e
Daniel Corn <d@cundd.net>
2015-11-02 16:35:05 +0100

    Fixed a critical bug in the debug method optimization

--- lib/ZetaPrints/mage-logging.php
+++ lib/ZetaPrints/mage-logging.php
@@ -1,19 +1,23 @@
 <?php
 
-function _zetaprints_debug ($msg = null) {
-  static $modernPhpVersion = version_compare(PHP_VERSION, '5.4.0') >= 0;
-  if ($modernPhpVersion) {
-    $backtrace = debug_backtrace(0, 2);
-  } else {
-  $backtrace = debug_backtrace();
-  }
+function _zetaprints_debug($msg = null)
+{
+    static $modernPhpVersion = -1;
+    if ($modernPhpVersion === -1) {
+        $modernPhpVersion = version_compare(PHP_VERSION, '5.4.0') >= 0;
+    }
+    if ($modernPhpVersion) {
+        $backtrace = debug_backtrace(0, 2);
+    } else {
+        $backtrace = debug_backtrace();
+    }
 
-  $callee_name = $backtrace[1]['function'];
+    $callee_name = $backtrace[1]['function'];
 
-  if (!$msg)
-    $msg = "function parameters:\n" . var_export($backtrace[1]['args'], true);
-  else if (is_array($msg))
-    $msg = "\n" . var_export($msg, true);
+    if (!$msg)
+        $msg = "function parameters:\n" . var_export($backtrace[1]['args'], true);
+    else if (is_array($msg))
+        $msg = "\n" . var_export($msg, true);
 
-  Mage::log("$callee_name: $msg");
+    Mage::log("$callee_name: $msg");
 }
