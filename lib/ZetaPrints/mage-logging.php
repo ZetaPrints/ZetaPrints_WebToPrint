45f5b845c459b1d456800baf919bbc7ad6266d9c
Daniel Corn <d@cundd.net>
2016-04-14 17:39:28 +0200

    Added meta information to the ZetaPrints debug method

--- lib/ZetaPrints/mage-logging.php
+++ lib/ZetaPrints/mage-logging.php
@@ -12,12 +12,30 @@ function _zetaprints_debug($msg = null)
         $backtrace = debug_backtrace();
     }
 
-    $callee_name = $backtrace[1]['function'];
+    $metaData = array(
+        'customerId' => null
+    );
 
-    if (!$msg)
+    $customerSession = Mage::getSingleton('customer/session');
+    if (is_object($customerSession) && $customerSession->isLoggedIn()) {
+        $customerData = $customerSession->getCustomer();
+        $metaData['customerId'] = is_object($customerData) ? $customerData->getId() : null;
+    }
+
+    $checkoutSession = Mage::getSingleton('checkout/session');
+    if (is_object($checkoutSession)) {
+        $metaData['quoteId'] = $checkoutSession->getQuoteId();
+    }
+
+    $calleeName = $backtrace[1]['function'];
+
+    if (!$msg) {
         $msg = "function parameters:\n" . var_export($backtrace[1]['args'], true);
-    else if (is_array($msg))
-        $msg = "\n" . var_export($msg, true);
+    } else {
+        if (is_array($msg)) {
+            $msg = "\n" . var_export($msg, true);
+        }
+    }
 
-    Mage::log("$callee_name: $msg");
+    Mage::log(sprintf('%s %s: %s', $calleeName, json_encode($metaData), $msg));
 }
